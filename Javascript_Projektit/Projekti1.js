518b7521fce85d65182dbc0edd982387e5612ba7
sailakivi <esko.sailakivi@edu.keuda.fi>
2023-11-08 13:32:39 +0200

    js

---
+++ Javascript_Projektit/Projekti1.js
@@ -0,0 +1,11 @@
+function laheta() {
+    var nimi = document.getElementById("").value;
+    var salasana = document.getElementById("").value;
+    var tietokone = document.getElementsByName("kone");
+    var kone = "";
+    var nettisivu = documentGetElementById("sivu").value;
+    var sivu = "";
+    var lempi = var lempi = document.getElementById("tunti");
+    var tunti = "";
+    var teksti =
+}
