ed4f8dc82750c2a4dcbd287932121fe20978cec3
sailakivi <esko.sailakivi@edu.keuda.fi>
2023-11-08 18:32:19 +0200

    1b

--- Javascript_Projektit/Projekti1.js
+++ Javascript_Projektit/Projekti1.js
@@ -1,46 +1,30 @@
 function laheta() {
-    var nimi = document.getElementById("").value;
-    var salasana = document.getElementById("").value;
-    var tietokone = document.getElementsByName("kone");
-    var kone = "";
-    var nettisivu = documentGetElementById("sivu").value;
-    var sivu = "";
-    var lempi = document.getElementById("tunti");
-    var tunti = "";
-    var teksti = document.getElementById("teksti");
+	let nimi = document.getElementById("nimi").value;
+	let salasana = document.getElementById("salasana").value;
+	tietokone = document.querySelector('input[name="tietokone"]:checked');
+	nettisivu = document.querySelector('input[name="nettisivu"]:checked');
+	let teksti = document.getElementById("teksti").value;
+	let tunti = document.getElementById("tunti").value;
 
-    for (var k = 0; k < tietokone.length; k++) {
-        if (tietokone[k].checked) {
-            kone = tietokone[k].value;
-        }
-    }
-    for (var t = 1; t < nettisivu.length; t++) {
-        if (nettisivu[t].selected) {
-            sivu = nettisivu[t].value;
-        }
-    }
-    for (var t = 1; a < lempi.length; t++) {
-        if (lempi[a].selected) {
-            tunti = lempi[a].value;
-        }
-
-    }
-    if (nimi.length < 1) {
-        alert("Anna nimesi, kiitos!")
-    }
-    else if (salasana.legth < 1) {
-        alert("Anna salasana, kiitos!")
-    }
-    else if (kone.length < 1) {
-        alert("Valitse millainen koneenkäyttäjä olet")
-    }
-    else if (sivu.length < 1) {
-        alert("Kerro miten hyvin osaat tehdä nettisivuja")
-    }
-    else if (tunti.length < 1) {
-        alert("Kerro lempituntisi koulussa, kiitos!")
-    }
-    else if (document.getElementById("teksti").value.trim() == "") {
-        alert("Kerro lyhyesti miten parantaisit opiskeluja, kiitos!")
-    }
+	if (nimi == "") {
+		alert("Anna nimi");
+	}
+	else if (salasana == "") {
+		alert("Anna salasana");
+	}
+	else if (!tietokone) {
+		alert("Valitse, millainen tietokoneen käyttäjä olet");
+	}
+	else if (!nettisivu) {
+		alert("Valitse, miten opit");
+	}
+	else if (tunti == "") {
+		alert("Valitse lempiaineesi");
+	}
+		else if (teksti.length == 0) {
+		alert("Kirjoita");
+	}
+	else {
+		alert("Kiitos vastauksestasi");
+	}
 }
