273b8389b76df8c52aa95716843c26d744c30a2f
sailakivi <esko.sailakivi@edu.keuda.fi>
2023-11-22 14:26:10 +0200

    p6

---
+++ Javascript_Projektit/Projekti6.js
@@ -0,0 +1,11 @@
+function tallenna() {
+    let etunimi = document.getElementById("enimi").value;
+    let sukunimi = document.getElementById("snimi").value;
+    let lähiosoite = document.getElementById("osoite").value;
+    let postinumero = document.getElementById("pnumero").value;
+    let postitoimipaikka = document.getElementById("ppaikka").value;
+    let puhelin = document.getElementById("puhelin").value;
+    let sähköposti = document.getElementById("sposti").value;
+
+    
+}
