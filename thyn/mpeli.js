2ea3793629c108c9fd1ea7d4ca5680169d936b9c
Thy Huynh <thy-huynh@hotmail.com>
2023-12-06 14:29:42 +0200

    P5

--- thyn/mpeli.js
+++ thyn/mpeli.js
@@ -8,23 +8,30 @@ const cards = document.querySelectorAll('.memory-card, .memory-card2');
     let second = 0; 
     let minute = 0; 
     let hour = 0;
+    let pairs = 0;
+    let maxPairs = 0;
+    let interval;
 
-    let interval = setInterval(function(){
-      timer.innerHTML = (minute+": "+second+"");
-      second++;
-     if(second == 60){
-        minute++;
-        second=0;
-     }
-     if(minute == 60){
-        hour++;
-        minute = 0;
-     }
-    },1000);
+    //MUUTTUJA VALIKKO EI OO ALUKS MITÄÄN
+    let valikko;
 
-    function stop() {
-        clearInterval(interval);
+    if (timer) {
+          interval = setInterval(function(){
+          timer.innerHTML = (minute+": "+second+"");
+          second++;
+         if(second == 60){
+            minute++;
+            second=0;
+         }
+         if(minute == 60){
+            hour++;
+            minute = 0;
+         }
+        },1000);
     }
+
+
+
 
   function flipCard() {
    if (lockBoard) return;
@@ -45,8 +52,16 @@ const cards = document.querySelectorAll('.memory-card, .memory-card2');
   }
 
   function checkForMatch() {
+      console.log(pairs)
     let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
-    isMatch ? disableCards() : unflipCards();
+    if (isMatch) { 
+        disableCards();
+        pairs++;
+    }
+    else { 
+        unflipCards();
+    }
+    endOfGame();
   }
 
   function disableCards() {
@@ -77,18 +92,44 @@ const cards = document.querySelectorAll('.memory-card, .memory-card2');
 
 
 function laheta() {
-switch(document.getElementById('valikko').value)
+    // TÄSSÄ VALIKOLLE ANNETAAN ARVO 
+valikko = document.getElementById('valikko').value;
+
+// JA SWITCH CASESSA KÄYTETÄÄN SITÄ ARVOA
+switch(valikko)
 {
 case "2":
 window.location.replace("./4x4.html");
+maxPairs = 8;
 break;
 
 case "3":
 window.location.replace("./4x6.html");
+maxPairs = 12;
 break;
 
 case "4":
 window.location.replace("./6x6.html");
+maxPairs = 18;
 break;
   }
+}
+
+function endOfGame() {
+    //LOPPUPELISSÄ TARKISTETAAN SE VALIKON ARVO JA SEN MUKAAN ANNETAAN MUUTTUJALLE MAXPAIRS JOKO 8, 12 TAI 18
+    if(valikko == 2 || "2") {
+        maxPairs = 8;
+        console.log(maxPairs);
+    }
+    else if(valikko == "3" || 3) {
+        maxPairs = 12;
+        console.log(maxPairs);
+    }
+    else if(valikko == "4"|| 4) {
+        maxPairs = 18;
+        console.log(maxPairs);
+    }
+    if(pairs == maxPairs) {
+        clearInterval(interval);
+    }
 }
