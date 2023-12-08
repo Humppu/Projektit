2de6a73a1661a7313cb7a1d7ff7618194a2a7c64
Homam Al-Mehsen <homam.al-mehsen@edu.keuda.fi>
2023-12-08 11:43:10 +0200

    toimiva

--- Homam/P8.js
+++ Homam/P8.js
@@ -1,16 +1,14 @@
 
-
-
 function drag(e) {
     e.dataTransfer.setData("text/plain", e.target.id);
 }
 
 
 function allowDrop(e) {
-    e.preventDefault(); 
+    e.preventDefault();
 }
 
- 
+
 function drop(e) {
     e.preventDefault();
     var id = e.dataTransfer.getData("text/plain");
--- Javascript_Projektit/P8/P8.js
+++ Javascript_Projektit/P8/P8.js
@@ -1,3 +1,49 @@
+const item = document.querySelector(".cardTitle");
+
+item.addEventListener("dragstart", dragStart);
+
+function dragStart(e) {
+    e.dataTransfer.setData("text/plain", e.target.id);
+    setTimeout(() => {
+        e.target.classList.add("hide");
+    }, 0);
+}
+
+
+const boxes = document.querySelectorAll(".tehtava");
+
+boxes.forEach(tehtava => {
+    tehtava.addEventListener("dragenter", dragEnter)
+    tehtava.addEventListener("dragover", dragOver);
+    tehtava.addEventListener("dragleave", dragLeave);
+    tehtava.addEventListener("drop", drop);
+});
+
+
+function dragEnter(e) {
+    e.preventDefault();
+    e.target.classList.add("drag-over");
+}
+
+function dragOver(e) {
+    e.preventDefault();
+    e.target.classList.add("drag-over");
+}
+
+function dragLeave(e) {
+    e.target.classList.remove("drag-over");
+}
+
+function drop(e) {
+    e.target.classList.remove("drag-over");
+
+    const id = e.dataTransfer.getData("text/plain");
+    const draggable = document.getElementById(id);
+
+    e.target.appendChild(draggable);
+    draggable.classList.remove("hide");
+}
+
 
 
 
@@ -7,10 +53,10 @@ function drag(e) {
 
 
 function allowDrop(e) {
-    e.preventDefault(); 
+    e.preventDefault();
 }
 
- 
+
 function drop(e) {
     e.preventDefault();
     var id = e.dataTransfer.getData("text/plain");
