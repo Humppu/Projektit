1a01c93e50848b72fed2b187c7ea265038f0698e
Homam Al-Mehsen <homam.al-mehsen@edu.keuda.fi>
2023-12-08 11:19:38 +0200

    Päivitys

---
+++ Homam/P8.js
@@ -0,0 +1,18 @@
+
+
+
+function drag(e) {
+    e.dataTransfer.setData("text/plain", e.target.id);
+}
+
+
+function allowDrop(e) {
+    e.preventDefault(); 
+}
+
+
+function drop(e) {
+    e.preventDefault();
+    var id = e.dataTransfer.getData("text/plain");
+    e.currentTarget.appendChild(document.getElementById(id));
+}
