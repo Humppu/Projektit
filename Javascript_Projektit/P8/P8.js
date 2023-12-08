f04446a1edef42bc213a8fa5adaa970971298b02
Homam Al-Mehsen <homam.al-mehsen@edu.keuda.fi>
2023-12-08 11:23:19 +0200

    Päivitys

--- Javascript_Projektit/P8/P8.js
+++ Javascript_Projektit/P8/P8.js
@@ -1,27 +1,4 @@
 
-var boxes = document.querySelectorAll(".tehtava");
-
-boxes.forEach(tehtava => {
-    tehtava.addEventListener("dragenter", dragEnter)
-    tehtava.addEventListener("dragover", dragOver);
-    tehtava.addEventListener("dragleave", dragLeave);
-    tehtava.addEventListener("drop", drop);
-});
-
-
-function dragEnter(e) {
-    e.preventDefault();
-    e.target.classList.add("drag-over");
-}
-
-function dragOver(e) {
-    e.preventDefault();
-    e.target.classList.add("drag-over");
-}
-
-function dragLeave(e) {
-    e.target.classList.remove("drag-over");
-}
 
 
 function drag(e) {
@@ -33,7 +10,7 @@ function allowDrop(e) {
     e.preventDefault(); 
 }
 
-
+ 
 function drop(e) {
     e.preventDefault();
     var id = e.dataTransfer.getData("text/plain");
